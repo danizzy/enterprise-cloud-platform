50ccc1a702147de27c9ba6740c5a7e77bd591265
Daniel Onabiyi <danielonabiyi@Mac.ht.home>
2026-07-23 12:52:16 -0600

    Create ALB module with target group and listener

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,3 +39,24 @@ module "ecr" {
   environment = var.environment
 
 }
+
+module "alb" {
+
+  source = "./modules/alb"
+
+
+  project_name = var.project_name
+
+
+  environment = var.environment
+
+
+  vpc_id = module.networking.vpc_id
+
+
+  public_subnets = module.networking.public_subnets
+
+
+  security_group_id = module.security.alb_security_group_id
+
+}
